631427e3dbe089d5240907274d62339a13aa15a3
Benedikt Schwering <mail@bschwer.ing>
2025-05-27 23:09:57 +0200

    fix(adapter): fix schema override by removing old commands and omitting cached results

--- src/adapter/schema_override.py
+++ src/adapter/schema_override.py
@@ -1,6 +1,6 @@
 from src.adapter.agent_adapter import AgentAdapter
-from src.models.schema_override import SchemaType
 from dotenv import load_dotenv, find_dotenv
+from src.models.schema import SchemaType
 from src.core.agent import Agent
 import json, os
 
@@ -19,6 +19,7 @@ class SchemaOverrideAdapter(AgentAdapter):
                     'Get the schema of the Neo4J graph database.',
                     'You should always start with this command to understand the database.',
                 ],
+                omit_cache=True,
             )
             def database_schema():
                 match schema_type:
